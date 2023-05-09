e828865f715800e4cadc2413b527133ad106bd9d
GioZerini <123542650+GioZerini@users.noreply.github.com>
2023-05-09 11:43:59 +0200

    tool.c updated to avoid memory leaks

--- tool.c
+++ tool.c
@@ -174,46 +174,37 @@ int ndpi_predict_linear(u_int32_t *values, u_int32_t num_values, u_int32_t predi
 void testTool() {
 	int i;
   FILE *fp;
-  char *memTotal, *memFree, *buffers, *cached;
-  u_int32_t *mem;
-	
-  memTotal = malloc(sizeof(char)*15);
-  memFree = malloc(sizeof(char)*15);
-  buffers = malloc(sizeof(char)*15);
-  cached = malloc(sizeof(char)*15);
-
-  mem = malloc(sizeof(u_int32_t)*15);
+  char memTotal[15], memFree[15], buffers[15], cached[15];
+  u_int32_t mem[10];
 
 	
   for(i=0; i<10; i++){
   	fp = popen("grep MemTotal /proc/meminfo | awk '{print $2}'", "r");
   	if(fgets(memTotal, sizeof(memTotal), fp) == NULL)
 			return;
-
+		pclose(fp);
 		fp = popen("grep MemFree /proc/meminfo | awk '{print $2}'", "r");
   	if(fgets(memFree, sizeof(memFree), fp) == NULL)
 			return;
-
+		pclose(fp);
 		fp = popen("grep Buffers /proc/meminfo | awk '{print $2}'", "r");
   	if(fgets(buffers, sizeof(buffers), fp) == NULL)
 			return;
-
+		pclose(fp);
 		fp = popen("grep Cached /proc/meminfo | awk '{print $2}'", "r");
   	if(fgets(cached, sizeof(cached), fp) == NULL)
 			return;
-
+    pclose(fp);
     mem[i] = atoi(memTotal) - atoi(memFree) - atoi(buffers) - atoi(cached);
 
-    sleep(10);
+    //sleep(10);
   }
-
-	pclose(fp);
 		
   for(i=0; i<10; i++){
     printf("%d KB\n", mem[i]);
 	}
 
-	u_int32_t predicted_value;
+  u_int32_t predicted_value;
 	float c, m;
 	int ret_val;
 	ret_val = ndpi_predict_linear(mem, 10, 6, &predicted_value, &c, &m);
@@ -225,12 +216,6 @@ void testTool() {
 	printf("The y-intercept is: %f\n", c);
 	printf("The slope is: %f\n", m);
 	printf("The predicted value is: %d\n", predicted_value);
-	
-	free(memTotal);
-	free(memFree);
-	free(buffers);
-	free(cached);
-	free(mem);
 }
 
 int main(int argc, char *argv[]){
